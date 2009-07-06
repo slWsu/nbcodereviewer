defd60afc561874a6ed5e1e90ade2dd4dac3866f
genewoo <genewoo@52afd42a-8230-0410-8988-bdfd7762c6b7>
2009-07-06 02:19:44 +0000

    initial project with empty project file. should change a lot here. -Gene

---
+++ plugins/trunk/model/src/test/java/com/taobaoprototype/model/UserTest.java
@@ -0,0 +1,37 @@
+package com.taobaoprototype.model;
+
+import java.io.InputStream;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import junit.framework.TestCase;
+
+import org.hibernate.ejb.HibernateEntityManager;
+
+public class UserTest extends TestCase
+{
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("model-test");
+
+    public void setUp() throws Exception
+    {
+        InputStream testData = User.class.getResourceAsStream("/user.db.xml");
+
+        HibernateEntityManager em = (HibernateEntityManager) emf.createEntityManager();
+
+        DbUnitDataLoader loader = new DbUnitDataLoader(testData, em.getSession().connection());
+
+        loader.populateTestData();
+    }
+
+    public void testFindAll()
+    {
+        User.setEntityManager(emf.createEntityManager());
+
+        User user = User.find(1);
+
+        assertNotNull(user);
+        assertEquals(1, user.getId());
+        assertEquals("John Doe", user.getName());
+    }
+}
